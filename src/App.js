8f86807f75c16e95bf726d051f6235c16cd25c76
Dominykas Seputis <dom.seputis@gmail.com>
2018-09-26 21:01:57 +0300

    Initiual commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <p>Nutri Starter</p>
+    </div>
+  </Router>
+);
+
+export default App;
