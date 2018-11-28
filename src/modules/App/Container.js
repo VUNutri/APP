1e494770142b1a3bec02a3cf778798a320ca012d
Žygimantas <zyg.sid2@gmail.com>
2018-11-28 19:15:23 +0200

    Some updates for main oage.

--- src/modules/App/Container.js
+++ src/modules/App/Container.js
@@ -2,57 +2,145 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './Container.css';
 import $ from 'jquery';
+import veganLogo from '../img/vegan.png';
+import vegetarianLogo from '../img/vegetarian.png';
+import gurmanLogo from '../img/gurman.png';
+import meatLogo from '../img/meat.png';
+import sweetsLogo from '../img/sweet.png';
+import snacksLogo from '../img/snacks.png';
 
 class Container extends React.Component {
-    handleClick() {
-            $('#sidebar, #content').toggleClass('active');
-            $('.collapse.in').toggleClass('in');
-            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    }
-  
-    render() {
-      return (
-        <Router>
-                <div id="content">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <div className="container-fluid">
+  handleClick() {
+    $('#sidebar, #content').toggleClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  }
 
-                            <button type="button" id="sidebarCollapse" className="btn btn-info" onClick={this.handleClick}>
-                                <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
-                                <span>Meniu</span>
-                            </button>
-                            <button className="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <i className="fas fa-align-justify"></i>
-                            </button>
+  render() {
+    return (
+      <Router>
+        <div id="content">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              <button
+                type="button"
+                id="sidebarCollapse"
+                className="btn btn-info"
+                onClick={this.handleClick}
+              >
+                <span
+                  className="glyphicon glyphicon-search"
+                  aria-hidden="true"
+                />
+                <span>Meniu</span>
+              </button>
+              <button
+                className="btn btn-dark d-inline-block d-lg-none ml-auto"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <i className="fas fa-align-justify" />
+              </button>
 
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul className="nav navbar-nav ml-auto">
-                                    <li className="nav-item active">
-                                        <a className="nav-link" href="#">Kazkas</a>
-                                    </li>
-                                    <li claclassNamess="nav-item">
-                                        <a className="nav-link" href="#">Kazkas</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Kazkas</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Kazkas</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
-
-
-                    Dabar reikia sugalvoti ka cia inpisti
-
-                    
-                    </div>
-
-        </Router>
-      );
-    }
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <ul className="nav navbar-nav ml-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">
+                      Kazkas
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Kazkas
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Kazkas
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Kazkas
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div className="card">
+            <div className="card-header">
+              SVEIKI ATVYKĘ!
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">VU NUTRI - KĄ MES SIŪLOME?</h5>
+              <p className="card-text">Sudarykite savo savaitės patiekalų pasirinkimą ir mes visą kitą atliksime už Jus!</p>
+              <a href="#" className="btn btn-primary">Sužinoti daugiau</a>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-4">
+              <div className="card">
+                <img className="card-img-top" src={veganLogo} alt="Vegan"/>
+                <div className="card-body">
+                  <p className="card-text">Veganiški patiekalai</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="card">
+                <img className="card-img-top" src={vegetarianLogo} alt="Vegetarian"/>
+                <div className="card-body">
+                  <p className="card-text">Vegetariški patiekalai</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="card">
+                <img className="card-img-top" src={gurmanLogo} alt="Gurman"/>
+                <div className="card-body">
+                  <p className="card-text">Gurmaniški patiekalai</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="card">
+                <img className="card-img-top" src={sweetsLogo} alt="Sweets"/>
+                <div className="card-body">
+                  <p className="card-text">Saldumynai</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img className="card-img-top" src={meatLogo} alt="Meat"/>
+                <div className="card-body">
+                  <p className="card-text">Mėsos mylėtojams</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img className="card-img-top" src={snacksLogo} alt="Snacks"/>
+                <div className="card-body">
+                  <p className="card-text">Užkandžiai</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default Container;
--- src/modules/App/Navbar.js
+++ src/modules/App/Navbar.js
@@ -3,62 +3,65 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../img/logo.png';
 
-const Navbar = () => (
-  <Router>
-            <nav id="sidebar">
-            <div className="sidebar-header">
-                <img src={logo} className="navLogo"/>
-            </div>
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {userLoggedIn:false};
+  }
 
-            <ul className="list-unstyled components">
-                <p>MENIU</p>
-                <li className="active">
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">MANO SAVAITĖ</a>
-                    <ul className="collapse list-unstyled" id="homeSubmenu">
-                        <li>
-                            <a href="#">Pirmadienis</a>
-                        </li>
-                        <li>
-                            <a href="#">Antradienis</a>
-                        </li>
-                        <li>
-                            <a href="#">Trečiadienis</a>
-                        </li>
-                        <li>
-                            <a href="#">Ketvirtadienis</a>
-                        </li>
-                        <li>
-                            <a href="#">Penktadienis</a>
-                        </li>
-                        <li>
-                            <a href="#">Šeštadienis</a>
-                        </li>
-                        <li>
-                            <a href="#">Sekmadienis</a>
-                        </li>
-                    </ul>
+  render() {
+    return (
+      <Router>
+        <nav id="sidebar">
+          <div className="sidebar-header">
+            <img alt="Nutri logo" src={logo} className="navLogo"/>
+          </div>
+          <ul className="list-unstyled components">
+            <p>MENIU</p>
+            <li className="active">
+              <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">PATIEKALAI</a>
+              <ul className="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                  <a href="#">Veganiški</a>
                 </li>
                 <li>
-                    <a href="#">APIE NUTRI</a>
+                  <a href="#">Saldūs</a>
                 </li>
                 <li>
-                    <a href="#">RECEPTAI</a>
+                  <a href="#">Gurmaniški</a>
                 </li>
                 <li>
-                    <a href="#">KONTAKTAI</a>
-                </li>
-            </ul>
-
-            <ul className="list-unstyled CTAs">
-                <li>
-                    <a href="#" className="navLogin">PRISIJUNGTI</a>
+                  <a href="#">Mėsos mylėtojai</a>
                 </li>
                 <li>
-                    <a href="#" className="navRegister">REGISTRUOTIS</a>
+                  <a href="#">Vegetariški</a>
                 </li>
-            </ul>
+                <li>
+                  <a href="#">Užkandžiai</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">APIE NUTRI</a>
+            </li>
+            <li>
+              <a href="#">RECEPTAI</a>
+            </li>
+            <li>
+              <a href="#">KONTAKTAI</a>
+            </li>
+          </ul>
+          <ul className="list-unstyled CTAs">
+            <li>
+              <a href="#" className="navLogin">PRISIJUNGTI</a>
+            </li>
+            <li>
+              <a href="#" className="navRegister">REGISTRUOTIS</a>
+            </li>
+          </ul>
         </nav>
-  </Router>
-);
+      </Router>);
+  }
+}
 
 export default Navbar;
