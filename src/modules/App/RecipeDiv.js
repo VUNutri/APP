3d62e9c52b0beb60849f5a493ccc4398ab1240aa
Žygimantas <zyg.sid2@gmail.com>
2018-12-11 01:53:20 +0200

    Now we are able to get all the recipes from the API. Created components for editing to represent every meal available

--- src/modules/App/RecipeDiv.js
+++ src/modules/App/RecipeDiv.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './Container.css';
 import './App.css';
-import $ from 'jquery';
+import './RecipeDiv.css';
 
 
-const RecipeDiv = () => (
-  <div>
-    Labas
-  </div>
-);
+function RecipeDiv(props) {
+  return (
+    <div>
+      <li className="list-group-item">
+        <div className="row">
+          <div className="col col-md-3">
+            <img src={props.image} alt="Meal" className="meal-img" />
+          </div>
+          <div className="col col-md-7">
+            Pavadinimas: <b>{props.title}</b>
+            <br />
+            ID: {props.id}
+            <br/>
+            {props.instructions}
+          </div>
+          <div className="col col-md-2">
+            <button type="button" class="btn btn-primary">Plačiau</button>
+          </div>
+        </div>
+      </li>
+    </div>
+  );
+}
 
 export default RecipeDiv;
--- src/modules/App/recipePage.js
+++ src/modules/App/recipePage.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './Container.css';
 import './App.css';
 import $ from 'jquery';
 import RecipeDiv from './RecipeDiv';
-import vegan from '../img/vegan.png';
+
 
 class recipePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { meals: [] };
+  }
 
   handleClick() {
     $('#sidebar, #content').toggleClass('active');
@@ -14,8 +19,20 @@ class recipePage extends React.Component {
     $('a[aria-expanded=true]').attr('aria-expanded', 'false');
   }
 
-  render ()
+  componentDidMount() {
+    axios.get(`https://cors.io/?http://35.177.42.215:8888/v1/api/recipes/getAll`)
+      .then((res) => {
+        const meals = res.data;
+        this.setState({ meals });
+      });
+
+  }
+
+  render()
   {
+    const allMeals = this.state.meals.map((meal,index) =>
+      <RecipeDiv title={meal.title} id={meal.id} image={meal.image} instructions={meal.instructions} />
+    );
     return (
       <div id="content">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -31,7 +48,9 @@ class recipePage extends React.Component {
           </div>
         </nav>
         <h1>Receptai</h1>
-        <RecipeDiv />
+        <ul className="list-group">
+          {allMeals}
+        </ul>
       </div>
     )
   }
